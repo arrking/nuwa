aba4c2d54e87ab574d1c854863d36965e2081058
nihgwu <nihgwu@live.com>
2015-06-03 00:22:34 +0800

    #6 initiate tabs for client

--- nuwa/www/js/app.js
+++ nuwa/www/js/app.js
@@ -1,9 +1,25 @@
-'use strict';
-angular.module('main', [
-  'ionic',
-  'ui.router',
-  // TODO: load other modules selected during generation
-])
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleLightContent();
+    }
+  });
+})
 
 .config(function($stateProvider, $urlRouterProvider) {
 
@@ -17,7 +33,7 @@ angular.module('main', [
     .state('tab', {
     url: "/tab",
     abstract: true,
-    templateUrl: "main/templates/tabs.html"
+    templateUrl: "templates/tabs.html"
   })
 
   // Each tab has its own nav history stack:
@@ -26,7 +42,7 @@ angular.module('main', [
     url: '/dash',
     views: {
       'tab-dash': {
-        templateUrl: 'main/templates/tab-dash.html',
+        templateUrl: 'templates/tab-dash.html',
         controller: 'DashCtrl'
       }
     }
@@ -36,7 +52,7 @@ angular.module('main', [
       url: '/chats',
       views: {
         'tab-chats': {
-          templateUrl: 'main/templates/tab-chats.html',
+          templateUrl: 'templates/tab-chats.html',
           controller: 'ChatsCtrl'
         }
       }
@@ -45,7 +61,7 @@ angular.module('main', [
       url: '/chats/:chatId',
       views: {
         'tab-chats': {
-          templateUrl: 'main/templates/chat-detail.html',
+          templateUrl: 'templates/chat-detail.html',
           controller: 'ChatDetailCtrl'
         }
       }
@@ -55,7 +71,7 @@ angular.module('main', [
     url: '/account',
     views: {
       'tab-account': {
-        templateUrl: 'main/templates/tab-account.html',
+        templateUrl: 'templates/tab-account.html',
         controller: 'AccountCtrl'
       }
     }

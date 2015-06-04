9a6bdcfda5332a2da84b31bacc3e6339211294e0
nihgwu <nihgwu@live.com>
2015-06-04 23:42:35 +0800

    #29 setup .jsbeautifyrc

--- client/app/main/controllers/start.js
+++ client/app/main/controllers/start.js
@@ -1,9 +1,9 @@
 'use strict';
 angular.module('main')
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function () {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function ($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,18 +11,18 @@ angular.module('main')
   //
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-  
+
   $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
+  $scope.remove = function (chat) {
     Chats.remove(chat);
-  }
+  };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
+.controller('AccountCtrl', function ($scope) {
   $scope.settings = {
     enableFriends: true
   };
